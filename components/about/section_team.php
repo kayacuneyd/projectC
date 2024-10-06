3438d1c4aa980d0e09f6c10ffe1ab9a27f425e8b
Cüneyt Kaya <kayacuneyd@gmail.com>
2024-10-06 14:31:42 +0200

    few changes

--- components/about/section_team.php
+++ components/about/section_team.php
@@ -16,5 +16,27 @@
                 </div>
             </div>
         </div>
+
+        <div class="col-xl">
+            <div class="card border-0 mt-xl-5">
+                <div class="card-body p-4 py-lg-5 bg-ck-light rounded">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <div class="text-center">
+                            <div class="h5 fw-bolder color-ck-dark">Have more questions?</div>
+                            <p class="text-muted mb-4">
+                                Contact me at
+                                <br />
+                                <a class="color-ck-dark" href="mailto:info@kayacuneyt.com">info@kayacuneyt.com</a>
+                            </p>
+                            <div class="h6 fw-bolder color-ck-dark">Follow me on social networks</div>
+                            <a class="fs-5 px-2" href="https://x.com/cueneytkayawebd"><i class="bi-twitter color-ck-dark"></i></a>
+                            <a class="fs-5 px-2" href="https://www.facebook.com/cueneytkayawebd"><i class="bi-facebook color-ck-dark"></i></a>
+                            <a class="fs-5 px-2" href="https://www.instagram.com/cueneytkayawebd/"><i class="bi-instagram color-ck-dark"></i></a>
+                            <a class="fs-5 px-2" href="https://www.linkedin.com/in/kayacuneyd2019/"><i class="bi-linkedin color-ck-dark"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
--- components/head.php
+++ components/head.php
@@ -71,6 +71,7 @@ function renderHead($page)
         
         <!-- CSS Stylesheets -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <link href="./assets/css/styles.css" rel="stylesheet" />
     </head>
     <body data-page="' . $page . '" class="d-flex flex-column h-100 bg-light">';
